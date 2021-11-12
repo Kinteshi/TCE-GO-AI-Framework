bcc582ca9dc348a7b080c373194906e1c4a8d438
Jeferson Marques <kinteshi@outlook.com>
2021-11-12 15:47:11 -0300

    v0.7.1
    * Correção no fluxo de extração, filtros temporariamente desabilitados

--- src/tcegoframework/flows/extraction.py
+++ src/tcegoframework/flows/extraction.py
@@ -1,21 +1,14 @@
 
 import warnings
 
-from pandas.core.frame import DataFrame
-from tcegoframework.dremio import construct_query, execute_query
+from tcegoframework.dremio import get_train_data
 from tcegoframework.preprocessing.text import regularize_columns_name
 
 warnings.filterwarnings('ignore')
 
-def query_dataset(filters: dict) -> DataFrame:
-    query = construct_query(filters)
-    data = execute_query(query)
-    return data
-
-
 def extraction_flow(filters: dict):
     print('Preparando e executando consulta...')
-    data = query_dataset(filters)
+    data = get_train_data()
     data = data.reset_index(drop=True)
     data = regularize_columns_name(data)
     print('Salvando base de dados...')
